680fd99f0213cfce37334cedefd268e5da5385ad
Administrator <1137448164@qq.com>
2016-10-12 17:09:39 +0800

    55661

--- MainActivity.java
+++ MainActivity.java
@@ -19,7 +19,7 @@ import com.example.administrator.searchdata.db.SearchHistoryDaoImpl;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-////11111
+////111111
     SearchView searchView;
     ListView listView;
     TextView clear;
